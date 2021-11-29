1f823372429df544e73fc1cbe360675766b9cb1a
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-11-29 08:27:01 -0800

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < applicationController
 
+    #renders login page (form)
+    get 'users/login' do
+        erb :users/login
+
+    end
+
+    #renders signup page (form)
+    get 'users/signup' do
+        erb :users/signup
+
+    end
+
+
+
 end
 
