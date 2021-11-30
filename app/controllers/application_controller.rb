a66fdcbbb7e8e4fb397d34289aec9950e1caf025
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-11-29 20:23:54 -0800

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    set :session_secret, "added_security_prease" 
   end
 
   get "/" do
