3662b5a2bbbacae666b29e8a32af53060f0c27b6
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-11-29 20:24:45 -0800

    add show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,42 @@
 class UsersController < ApplicationController
 
-    #renders login page (form)
+         #renders login page (form)
     get '/login' do
         erb :'login'
-      end
+    end
 
-    #log in user and create a session
-      post '/login' do
-        #find the user
-        @user = User.find_by(email: params[:email])
-        #authenticate (make sure they have the right credentials)
-        if @user.authenticate(params[:password])
-            binding.pry
-          #log the user in -- create session
-          #redirect to (show, dashboard, home
+              #log in user and create a session
+    post '/login' do
+              #find the user
+      @user = User.find_by(email: params[:email])
+              #authenticate (make sure they have the right credentials)
+      if @user.authenticate(params[:password])
+         session[:user_id] = @user.id #actually logging in
+              #log the user in -- create session
+              #redirect to (show, dashboard, home
+              puts session
+         redirect "users/#{@user.id}"
         else
             #tell user they entered incorrect credentials
             #redirect to login page
-
-        end
-
+        
+      end
     end
 
-    
-    
-      #renders signup page (form)
+    #renders signup page (form)
     get 'users/signup' do
         erb :'users/signup'
 
     end
 
+    #user show route
+    get '/users/:id' do
+      "this will be the user show route"
+    end
+
 
 
 end
+
 
 
