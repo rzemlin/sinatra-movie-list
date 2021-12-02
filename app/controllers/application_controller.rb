21c4cf1a8a732a58ae3d0c42644fd8a5c334c6af
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-12-01 21:43:28 -0800

    add sinatra::flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "added_security_prease" 
+    register Sinatra::Flash
+
   end
 
   get "/" do
