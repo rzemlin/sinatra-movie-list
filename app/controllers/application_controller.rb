6cb44406136486017badcb1a2073fdcc80263bcb
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-12-06 08:03:26 -0800

    set flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,14 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+
+    else
+      erb :welcome
+
+    end
+
   end
 
   helpers do
